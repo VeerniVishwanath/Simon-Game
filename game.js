e35fee789eaa37ac4ac0b6ecb50fa1fadd309b4f
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-06-16 17:59:29 +0530

    Make Responsive

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+maxWidth();
+function maxWidth() {
+  if (window.matchMedia("(max-width: 1120px)").matches) {
+    $("#level-title").text("Press Start");
+  }
+}
+
 // User Clicked Pattern
 var userClickedPattern = [];
 
@@ -11,13 +18,25 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var started = false;
 var level = 0;
 $(document).keydown(function () {
+  checkStart();
+});
+
+// For mobile Users
+$(".button-mobile").click(function () {
+  checkStart();
+  $(".button-mobile").css("display", "none");
+});
+
+// Checks start of the Game
+function checkStart() {
   if (!started) {
     nextSequence();
     $("level-title").text("Level " + level);
     started = true;
   }
-});
+}
 
+// Next color Sequence
 function nextSequence() {
   userClickedPattern = [];
   level++;
@@ -81,4 +100,6 @@ function startOver() {
   level = 0;
   started = false;
   gamePattern = [];
+  $(".button-mobile").css("display", "");
+  $(".button-mobile").text("Restart");
 }
